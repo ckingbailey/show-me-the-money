e8cc315cae03fd85fcaf67af7df653959b0c1f82
ckb <ckingbailey@gmail.com>
2022-07-22 09:41:16 -0700

    separate get_ fcns from dataframification fcns

--- v2_api/create_socrata_csv.py
+++ v2_api/create_socrata_csv.py
@@ -16,11 +16,19 @@ Socrata required fields:
   "party",
 ]
 """
+import json
 from pathlib import Path
 import pandas as pd
 import requests
+from .query_v2_api import get_filer
 
-CONTRIBUTION_FORM = 'F460A'
+FILER_TO_CAND_PATH = 'filer_to_candidate.csv'
+SOCRATA_CONTRIBS_SCHEMA_PATH = 'socrata_schema_contrib_fields.json'
+SOCRATA_EXPEND_SCHEMA_PATH = 'socrata_schema_expend_fields.json'
+EXAMPLE_DATA_DIR = 'example'
+OUTPUT_DATA_DIR = 'output'
+
+CONTRIBUTION_FORMS = [ 'F460A', 'F460C' ]
 EXPENDITURE_FORM = 'F460E'
 BASE_URL = 'https://netfile.com/api/campaign'
 PARAMS = { 'aid': 'COAK' }
@@ -46,7 +54,19 @@ retry_strategy = requests.adapters.Retry(total=5, backoff_factor=1)
 adapter = TimeoutAdapter(max_retries=retry_strategy)
 session.mount('https://', adapter)
 
-def get_filings(offset=0) -> tuple[pd.DataFrame, dict]:
+def select_response_meta(response_body):
+    """ Get props needed from response body for further requests """
+    return {
+        'page_number': response_body['pageNumber'],
+        'has_next_page': response_body['hasNextPage'],
+        'total': response_body['totalCount'],
+        'count': response_body['count'],
+        'limit': response_body['limit'],
+        'offset': response_body['offset'],
+        'next_offset': response_body['limit'] + response_body['offset'] if response_body['hasNextPage'] else None
+    }
+
+def get_filings(offset=0) -> tuple[pd.DataFrame, list[dict], dict]:
     """ Get a page of filings
         Return fields required by Socrata
     """
@@ -58,43 +78,13 @@ def get_filings(offset=0) -> tuple[pd.DataFrame, dict]:
     res = session.get(f'{BASE_URL}/filing/v101/filings', params=params, auth=AUTH)
     body = res.json()
 
-    return pd.DataFrame([
-        {
-            'filer_id': result['filerMeta']['filerId'],
-            'filer_name': result['filerMeta']['commonName'],
-            'filing_nid': result['filingNid'],
-            'receipt_date': result['calculatedDate']
-        } for result in body['results']
-    ]), {
-        'page_number': body['pageNumber'],
-        'has_next_page': body['hasNextPage'],
-        'total': body['totalCount'],
-        'count': body['count'],
-        'offset': body['offset'],
-        'next_offset': body['count'] + body['offset'] if body['hasNextPage'] else None
-    }
+    return body['results'], select_response_meta(body)
 
-def get_transactions(filing_nid, offset=0) -> tuple[pd.DataFrame, dict]:
+def get_transactions(filing_nid, offset=0) -> tuple[pd.DataFrame, list[dict], dict]:
     """ Get a page of transactions
         for a filingNid
         Return fields required by Socrata
     """
-    tran_cols = [
-        'filing_nid',
-        'contributor_name',
-        'contributor_type',
-        'contributor_address',
-        'contributor_location',
-        'amount',
-        'expenditure_type',
-        'expenditure_description',
-        'form',
-        'election_year',
-        'office',
-        'jurisdiction',
-        'party'
-    ]
-
     params = {
         **PARAMS,
         'filingNid': filing_nid,
@@ -107,40 +97,92 @@ def get_transactions(filing_nid, offset=0) -> tuple[pd.DataFrame, dict]:
     res = session.get(f'{BASE_URL}/cal/v101/transaction-elements', params=params, auth=AUTH)
     body = res.json()
 
+    return body['results'], select_response_meta(body)
+
+def get_all_trans_for_filing(filing_nid):
+    """ Get all transactions for a single filing_nid """
+    next_offset = 0
+    params = {
+        'filing_nid': filing_nid
+    }
+
+    transactions, meta = get_transactions(**params)
+    end = '/' if meta['total'] > meta['limit'] else ' '
+    if end != ' ':
+        print('')
+    print(meta['total'], end=end, flush=True)
+
+    next_offset = meta.get('next_offset')
+    while next_offset is not None:
+        results, meta = get_transactions(**params, offset=next_offset)
+        prog_char = '¡' if len(results) > 0 else '.'
+        transactions += results
+        print(prog_char, end='', flush=True)
+
+    return transactions
+
+def df_from_filings(filings):
+    """ Transform filings into Pandas DataFrame """
+    return pd.DataFrame([{
+        'filer_id': f['filerMeta']['filerId'],
+        'filer_name': f['filerMeta']['commonName'],
+        'filing_nid': f['filingNid'],
+        'receipt_date': f['calculatedDate']
+    } for f in filings ])
+
+def df_from_trans(transactions):
+    """ Transform transaction dict into Pandas DataFrame """
+    tran_cols = [
+        'filing_nid',
+        'contributor_name',
+        'contributor_type',
+        'contributor_address',
+        'contributor_location',
+        'amount',
+        'expn_code',
+        'expenditure_description',
+        'form',
+        'jurisdiction',
+        'party'
+    ]
+
     transaction_data = [
         {
-            'filing_nid': filing_nid,
-            'contributor_name': result['allNames'],
-            'contributor_type': 'Individual' if result['transaction']['entityCd'] == 'IND' else 'Organization',
-            'contributor_address': ' '.join([
-                str(result['addresses'][0].get('line1', '')),
-                str(result['addresses'][0].get('line2', '')),
-                str(result['addresses'][0].get('city', '')),
-                str(result['addresses'][0].get('state', '')),
-                str(result['addresses'][0].get('zip', ''))
-            ]).strip() if len(result['addresses']) > 0 else '',
+            'filing_nid': t['filingNid'],
+            'contributor_name': t['allNames'],
+            'contributor_type': 'Individual' if t['transaction']['entityCd'] == 'IND' else 'Organization',
+            'contributor_address': ', '.join([ ' '.join([
+                str(t['addresses'][0].get('line1')) or '',
+                str(t['addresses'][0].get('line2')) or '',
+                str(t['addresses'][0].get('city')) or ''
+            ]), ' '.join([
+                str(t['addresses'][0].get('state')) or '',
+                str(t['addresses'][0].get('zip')) or ''
+            ]) ]).strip() if len(t['addresses']) > 0 else '',
             'contributor_location': (
-                result['addresses'][0]['latitude'], result["addresses"][0]['longitude']
-             ) if len(result['addresses']) > 0 else tuple(),
-            'amount': result['calculatedAmount'],
-            'expenditure_type': result['transaction']['tranCode'],
-            'expenditure_description': result['transaction']['tranDscr'],
-            'form': result['calTransactionType'],
-            'election_year': None,
-            'office': None,
+                t["addresses"][0]['longitude'], t['addresses'][0]['latitude']
+             ) if len(t['addresses']) > 0 else tuple(),
+            'amount': t['calculatedAmount'],
+            'expn_code': t['transaction']['tranCode'],
+            'expenditure_description': t['transaction']['tranDscr'] or '',
+            'form': t['calTransactionType'],
             'jurisdiction': None,
             'party': None,
         }
-        for result in body['results']
+        for t in transactions
     ]
-    return pd.DataFrame(transaction_data, columns=tran_cols), {
-        'page_number': body['pageNumber'],
-        'has_next_page': body['hasNextPage'],
-        'total': body['totalCount'],
-        'count': body['count'],
-        'offset': body['offset'],
-        'next_offset': body['count'] + body['offset'] if body['hasNextPage'] else None
-    }
+
+    return pd.DataFrame(transaction_data, columns=tran_cols)
+
+def df_from_filers(filers):
+    """ Transform filers into Pandas DataFrame """
+    # filter out committees without CA SOS IDs
+    # as we have no way to join them to filings
+    return pd.DataFrame([ {
+        'filer_id': f['filerNid'],
+        'sos_id': f['registrations'].get('CA SOS')
+    } for f in filers if f['registrations'].get('CA SOS') is not None
+    ]).astype({ 'sos_id': 'string' })
 
 def main():
     """ Query Netfile results 1 page at a time
@@ -153,46 +195,79 @@ def main():
         3. Match filingDate to electionDate, extract year from date
         4. Query /filer/v101/filers/{filer_nid}, get electionInfluences[electionDate].seat.officeName
     """
-    filing_df, response_meta = get_filings()
+    print('===== Get filings =====')
+    filings, response_meta = get_filings()
     print(response_meta['total'])
-    print(filing_df.head())
 
     next_offset = response_meta['next_offset']
     end = ''
     while next_offset is not None:
-        filings, filing_meta = get_filings(offset=next_offset)
-        next_offset = filing_meta['next_offset']
-        filing_df = pd.concat([ filing_df, filings ])
+        results, meta = get_filings(offset=next_offset)
+        next_offset = meta['next_offset']
+        filings += results
         print('¡', end=end, flush=True)
+    print('')
+
+    filing_df = df_from_filings(filings)
 
     # Keep only filings for 2020
     # This is bogus because 2020 election filings may have been received in 2019
     # Get the filer_id => election_date mapping from Suzanne
     filing_df['receipt_date'] = pd.to_datetime(filing_df['receipt_date'])
-    filing_df = filing_df[filing_df['receipt_date'].dt.year == 2020]
+    filing_df = filing_df[filing_df['receipt_date'].dt.year > 2018]
 
+    print('===== Get transactions =====')
     filing_nids = set(filing_df['filing_nid'])
-    tran_df = pd.DataFrame()
+    transactions = []
     for filing_nid in filing_nids:
-        transactions, response_meta = get_transactions(filing_nid)
-        print(response_meta['total'])
-        print(transactions.head(3))
-        prog_char = '¡' if len(transactions.index) > 0 else '.'
-        tran_df = pd.concat([ tran_df, transactions ])
-        print(prog_char, end=end, flush=True)
-
-        next_offset = response_meta['next_offset']
-        # Get all pages of transactions
-        while next_offset is not None:
-            transactions, response_meta = get_transactions(filing_nid, offset=next_offset)
-            next_offset = response_meta['next_offset']
-            prog_char = '¡' if len(transactions.index) > 0 else '.'
-            tran_df = pd.concat([ tran_df, transactions ])
-            print(prog_char, end=end, flush=True)
-
+        transactions += get_all_trans_for_filing(filing_nid)
     print('')
 
-    df = filing_df.merge(tran_df, on='filing_nid')
+    print('===== Get filers =====')
+    filers = []
+    for filer_nid in set(filing_df['filer_id']):
+        filers += get_filer(filer_nid)
+        print('¡', end='', flush=True)
+    print('')
+
+    tran_df = df_from_trans(transactions)
+    filer_df = df_from_filers(filers)
+
+    expn_codes = pd.read_csv('expenditure_codes.csv').rename(columns={
+        'description': 'expenditure_type'
+    })
+    print('===== expn_codes dtypes', expn_codes.dtypes, sep='\n')
+    tran_df = tran_df.merge(expn_codes, how='left', on='expn_code')
+    print('===== tran_df dtypes =====', len(tran_df.index), tran_df.dtypes, sep='\n')
+
+    Path(f'{EXAMPLE_DATA_DIR}/filings.json').write_text(json.dumps(filings, indent=4), encoding='utf8')
+    Path(f'{EXAMPLE_DATA_DIR}/transactions_2019-present.json').write_text(
+        json.dumps(transactions, indent=4), encoding='utf8'
+    )
+    Path(f'{EXAMPLE_DATA_DIR}/filers_2019-present.json').write_text(json.dumps(filers, indent=4), encoding='utf8')
+
+    filer_to_cand_cols = [
+        'local_agency_id',
+        'sos_id',
+        'election_year',
+        'candidate',
+        'filer_name_local',
+        'office',
+        'start',
+        'end'
+    ]
+    df = pd.read_csv(FILER_TO_CAND_PATH)
+    df = df.rename(columns={
+        'SOS ID': 'sos_id',
+        'Local Agency ID': 'local_agency_id',
+        'Filer Name': 'filer_name_local',
+        'contest': 'office'
+    })[
+        filer_to_cand_cols
+    ].astype({ 'sos_id': 'string' })
+
+    df = df.merge(filer_df, how='left', on='sos_id')
+    df = df.merge(filing_df, how='left', on='filer_id').merge(tran_df, on='filing_nid')
     df = df.astype({
         'filer_name': 'string',
         'contributor_name': 'string',
@@ -200,39 +275,20 @@ def main():
         'contributor_address': 'string',
         'amount': float
     })
-    print(len(df.index), df.dtypes)
+    print('===== dtypes after merge =====', len(df.index), df.dtypes, sep='\n')
     pd.set_option('max_colwidth', 12)
     print(df.head(12))
 
-    df.to_csv('all_trans_raw.csv', index=False)
+    df.to_csv(f'{EXAMPLE_DATA_DIR}/all_trans.csv', index=False)
 
-    contrib_df = df[df['form'] == CONTRIBUTION_FORM]
-    contrib_cols = [
-        'filer_id',
-        'filer_name',
-        'contributor_name',
-        'contributor_type',
-        'contributor_address',
-        'amount',
-        'receipt_date'
-    ]
-    contrib_df[contrib_cols].to_csv('contribs_socrata.csv', index=False)
+    contrib_cols = json.loads(
+        Path(SOCRATA_CONTRIBS_SCHEMA_PATH).read_text(encoding='utf8')
+    ).keys()
+    contrib_df = df[df['form'].isin(CONTRIBUTION_FORMS)][contrib_cols]
+    print(contrib_df.head(), len(contrib_df.index), sep='\n')
+    contrib_df.to_csv(f'{OUTPUT_DATA_DIR}/contribs_socrata.csv', index=False)
 
-    expend_cols = [
-        'filer_id',
-        'filer_name',
-        'recipient_name',
-        'expenditure_type',
-        'recipient_address',
-        'recipient_location',
-        'expenditure_description',
-        'amount',
-        'expenditure_date',
-        'election_year',
-        'office',
-        'jurisdiction',
-        'party'
-    ]
+    expend_cols = json.loads(Path(SOCRATA_EXPEND_SCHEMA_PATH).read_text(encoding='utf8'))
     expend_df = df[df['form'] == EXPENDITURE_FORM].rename(columns={
         'contributor_name': 'recipient_name',
         'contributor_address': 'recipient_address',
@@ -240,7 +296,7 @@ def main():
         'receipt_date': 'expenditure_date'
     })[expend_cols]
     print(expend_df.head(), len(expend_df.index), sep='\n')
-    expend_df.to_csv('expends_socrata.csv', index=False)
+    expend_df.to_csv(f'{OUTPUT_DATA_DIR}/expends_socrata.csv', index=False)
 
 if __name__ == '__main__':
     main()
