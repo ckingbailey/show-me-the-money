ca9e0d5896ecbc4b1df7d7041c8d3d8aa8e69043
colin-mbp-2022 <colin@spartascience.com>
2022-08-11 18:56:09 -0700

    get more info about missing trans

--- v2api/count_trans.py
+++ v2api/count_trans.py
@@ -7,15 +7,32 @@ def main():
     # T Reid's filer_id
     reid_nf = netfile[
         (netfile['filer_name'] == 'Treva Reid') & (netfile['election_year'] == 2022)
-    ][['filer_id', 'tran_id']]
+    ]
 
     reid_id = reid_nf['filer_id'].iloc[0]
-    reid_pec = suzanne[suzanne['filer_id'] == reid_id]['tran_id']
+    reid_pec = suzanne[suzanne['filer_id'] == reid_id]
 
     unq_reid_trans_nf = set(reid_nf['tran_id'])
-    unq_reid_trans_pec = set(reid_pec)
+    unq_reid_trans_pec = set(reid_pec['tran_id'])
 
-    print(unq_reid_trans_pec - unq_reid_trans_nf)
+    missing_trans = unq_reid_trans_pec - unq_reid_trans_nf
+
+    print(missing_trans)
+
+    print(reid_pec[
+        reid_pec['tran_id'].isin(missing_trans)
+    ][
+        ['tran_id_unique', 'tran_id', 'tran_amt1', 'tran_date', 'tran_naml', 'report_num', 'rpt_date']
+    ].sort_values(
+        by=['tran_date', 'tran_id']
+    ))
+
+    all_trans = pd.read_csv('example/all_trans.csv')
+    print(all_trans[
+        all_trans['tran_id'].isin(missing_trans)
+    ][['filing_id', 'tran_id', 'amount', 'receipt_date', 'filing_date', 'contributor_name']].sort_values(
+        by=['filing_date', 'tran_id']
+    ))
 
 if __name__ == '__main__':
     main()
