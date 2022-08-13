b36339a1ad563a9ed21c4229a41c848e711aeb06
ckb <ckingbailey@gmail.com>
2022-08-13 11:13:00 -0700

    wip: did not find missing trans from full pull

--- v2api/get_all_trans_unqualified.py
+++ v2api/get_all_trans_unqualified.py
@@ -1,20 +1,34 @@
 import json
 from pathlib import Path
+import requests
 from .create_socrata_csv import AUTH, PARAMS, BASE_URL, session
 
 def main():
     url = f'{BASE_URL}/cal/v101/transaction-elements'
-    params = {
-        **PARAMS
-    }
 
     transactions = []
-    next_offset = True
+    bad_transactions = []
+    bad_transaction = None
+    next_offset = 0
+    has_next_page = True
     while next_offset is not None:
-        if type(next_offset) == int:
+        params = {
+            **PARAMS,
+            'parts': 'All',
+            'limit': 1000
+        }
+        if has_next_page:
             params['offset'] = next_offset
 
-        res = session.get(url, params=params, auth=AUTH)
+        try:
+            res = session.get(url, params=params, auth=AUTH)
+        except requests.HTTPError as exc:
+            print(exc, exc.args, exc.errno, exc.response)
+            exc_body = exc.response.json()
+            print(exc_body)
+            params.pop('parts')
+            res = session.get(url, params=params, auth=AUTH)
+
         body = res.json()
         if len(transactions) == 0:
             print('Total', body['totalCount'])
@@ -23,7 +37,8 @@ def main():
         print('▰', end='', flush=True)
         next_offset = body['offset'] + body['limit'] if body['hasNextPage'] is True else None
 
-    Path('example/unfiltered_transactions.json').write_text(json.dumps(transactions), encoding='utf8')
+    Path('example/unfiltered_transactions.json').write_text(
+        json.dumps(transactions), encoding='utf8')
 
 if __name__ == '__main__':
     main()
