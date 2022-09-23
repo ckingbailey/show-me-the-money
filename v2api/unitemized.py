bd8c30926e031c9a856fcb51acb309ec0b75c794
ckb <ckingbailey@gmail.com>
2022-09-23 11:48:34 -0700

    get multiple filing elements

--- v2api/unitemized.py
+++ v2api/unitemized.py
@@ -7,7 +7,7 @@ from .late_contributions import load_filings, load_from_file
 
 filing_elements = 'filing_elements'
 
-def get_filing_elements(filing_nid):
+def get_filing_elements_by_filing(filing_nid):
     """ Get filing-elements by filing_nid """
     path = '/filing/v101/filing-elements'
     params = {
@@ -19,9 +19,17 @@ def get_filing_elements(filing_nid):
     f_elements = res.json()
     return f_elements['results']
 
+def get_multiple_filing_elements(filings):
+    filing_elements = []
+    for f in filings:
+        filing_nid = f['filingNid']
+        filing_elements += get_filing_elements_by_filing(filing_nid)
+
+    return filing_elements
+
 def load_filing_elements():
     """ Load filing elements from disk """
-    return load_from_file(filing_elements)
+    return load_from_file(f'{filing_elements}.json')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -33,7 +41,7 @@ def main():
     filings = load_filings()
     four_sixties = [ f for f in filings if f['specificationRef']['name'] == 'FPPC460' ]
 
-    f_elements = (get_filing_elements(four_sixties[0]['filingNid'])
+    f_elements = (get_multiple_filing_elements(four_sixties[:10])
         if args.download
         else load_filing_elements())
     save_source_data({ filing_elements: f_elements })
