aa5145d3987cc2f9b8ad859c25aedc2cec8a6f76
ckb <ckingbailey@gmail.com>
2023-07-30 17:03:17 -0700

    checkout Filer from polars branch

--- model/Filer.py
+++ model/Filer.py
@@ -2,6 +2,11 @@
     each capable of returning a Pandas DataFrame
 """
 from datetime import datetime as dt
+from pprint import PrettyPrinter
+from typing import List
+import pandas as pd
+import polars as pl
+from .base import BaseModelCollection
 
 class Filer:
     """ A filer record """
@@ -9,18 +14,60 @@ class Filer:
         self.filer_nid = filer_record['filerNid']
         self.filer_id = filer_record['registrations'].get('CA SOS')
 
-        influence = filer_record['electionInfluence']
-        self.filer_name, self.office = self.get_filer_name(influence)
-        self.start_date = influence['startDate']
-        self.end_date = influence['endDate']
-        self.election_year = dt.strptime(influence['electionDate'], '%Y-%m-%d').year
+        influences = filer_record.get('electionInfluences')
 
-    def get_filer_name(self, election_influence):
+        filer_contest = self._get_filer_contest(influences)
+        self.filer_name, self.office, self.start_date, self.end_date, self.election_date = filer_contest
+
+
+    def _get_filer_contest(self, election_influences):
         """ Get filer name and office from election_influence object """
-        if election_influence['candidate']:
-            filer_name = election_influence['candidate']['candidateName']
-            office = election_influence['seat']['officeName']
-            return filer_name, office
-        elif election_influence['measure']:
-            # This currently appears to be broken/missing in the NetFile API
-            return None, None
+        for i in election_influences:
+            if i['candidate']:
+                candidate_name = i['candidate']['candidateName']
+
+                try:
+                    office_name = i['seat']['officeName']
+                except TypeError as e:
+                    if e.args[0] == "'NoneType' object is not subscriptable":
+                        office_name = i['candidate']['seatNid']
+                    else:
+                        raise e
+                finally:
+                    office_name = None
+                start_date = i['startDate']
+                end_date = i['endDate']
+                election_date = i['electionDate']
+
+                return candidate_name, office_name, start_date, end_date, election_date
+            elif i['measure']:
+                # This currently appears to be broken/missing in the NetFile API
+                return None, None, i['startDate'], i['endDate'], i['electionDate']
+
+        return [ None for _ in range(5) ]
+
+class FilerCollection(BaseModelCollection):
+    """ A bunch of filer objects """
+    @property
+    def df(self):
+        if self._df.empty:
+            self._df = pd.DataFrame([
+                filer.__dict__
+                for filer in self.collection
+            ]).astype({
+                'filer_nid': 'string',
+                'filer_id': 'string',
+                'filer_name': 'string',
+                'start_date': 'datetime64',
+                'end_date': 'datetime64',
+                'election_date': 'datetime64'
+            })
+
+        return self._df
+
+    def pl(self):
+        if self._pl.is_empty():
+            self._pl = pl.DataFrame([
+                filer.__dict__
+                for filer in self.collection
+            ])
